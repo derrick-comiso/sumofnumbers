85e3484c642f17a91e387d3264accc8f066aaa45
Derrick Comiso <dcomiso@hawaii.edu>
2016-09-18 21:26:35 -1000

    finished first function

---
+++ .idea/sumofnumbers.js
@@ -0,0 +1,29 @@
+/**
+ * Solving E26.
+ * Created by DerrickAllen on 9/18/2016.
+ */
+
+const testNums = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testNums));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testNums));
+
